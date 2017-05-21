ad41f96b5f87c1723d576d64f89ed63a8a91a59b
Kai Mysliwiec <kai@eightnine.de>
2017-05-21 18:06:52 +0200

    more slides more fonts more colors

--- src/main.js
+++ src/main.js
@@ -7,13 +7,20 @@ let doc = {
       bullets: ['Slide 1 bullet 1', 'Slide 1 bullet 2', 'Slide 1 bullet 3', 'Slide1one bullet 4', 'Slide 1 bullet 5', 'Slide 1 bullet 6 '] 
     }, {
       font: 'fonts/droid_sans_bold.typeface.json',
+      color: 0x22ff22,
       bullets: ['Slide 2 bullet 1', 'Slide 2 bullet 2', 'Slide 2 bullet 3', 'Slide 2 bullet 4', 'Slide 2 bullet 5', 'Slide 2 bullet 6 ']
     }, { 
       font: 'fonts/gentilis_bold.typeface.json',
+      color: 0xff22ff,
       bullets: ['Slide 3 bullet 1', 'Slide 3 bullet 2', 'Slide 3 bullet 3', 'Slide 3 bullet 4', 'Slide 3 bullet 5', 'Slide 3 bullet 6 ']
     }, { 
       font: 'fonts/optimer_bold.typeface.json',
+      color: 0xffff22,
       bullets: ['Slide 4 bullet 1', 'Slide 4 bullet 2', 'Slide 4 bullet 3', 'Slide 4 bullet 4', 'Slide 4 bullet 5', 'Slide 4 bullet 6 ']
+    }, { 
+      font: 'fonts/droid_sans_mono_regular.typeface.json',
+      color: 0x2222ff,
+      bullets: ['Slide 5 bullet 1', 'Slide 5 bullet 2', 'Slide 5 bullet 3', 'Slide 5 bullet 4', 'Slide 5 bullet 5', 'Slide 5 bullet 6 ']
     }
   ]
 };
